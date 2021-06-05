ad4e44b1cab99ec6018a5cf1bd76c69ef44a08a8
sina-amareh <79862130+sina-amareh@users.noreply.github.com>
2021-06-05 21:16:05 +0430

    edited user class for encapsulation

--- classes.py
+++ classes.py
@@ -3,7 +3,7 @@
 class User :
     def __init__(self, password, name, mail):
         self.__password, self.__name, self.__mail = password, name, mail
-        self.wallet = 0
+        self.__wallet = 0
     def setPassword(self, new_password):
         self.__password = new_password
 
