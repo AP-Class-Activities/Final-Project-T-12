b2a4c593acb647d7cf48ac4a788e49965e68bf5e
sina-amareh <79862130+sina-amareh@users.noreply.github.com>
2021-06-05 21:05:57 +0430

    forgot to use setter and getter !

--- classes.py
+++ classes.py
@@ -2,10 +2,10 @@
 
 class User :
     def __init__(self, password, name, mail):
-        self.password, self.name, self.mail = password, name, mail
+        self.__password, self.__name, self.__mail = password, name, mail
         self.wallet = 0
     def setPassword(self, new_password):
-        self.password = new_password
+        self.__password = new_password
 
 class Seller(User):
   pass
