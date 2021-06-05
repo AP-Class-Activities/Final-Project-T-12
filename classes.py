6dd031eb47e70dcf8465518cc63b84fc8b4f5081
sina-amareh <79862130+sina-amareh@users.noreply.github.com>
2021-06-05 18:18:07 +0430

    User Class done!

--- classes.py
+++ classes.py
@@ -1,8 +1,11 @@
 # main classes of the project
 
-class User:
-  def __init__(self):
-    pass
+class User :
+    def __init__(self, password, name, mail):
+        self.password, self.name, self.mail = password, name, mail
+        self.wallet = 0
+
+
 class Seller(User):
   pass
 class Customer(User):
