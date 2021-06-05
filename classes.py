1695781ea11064727010fa845e79ac6f84689c93
behbod-eskafi <80491468+behbod-eskafi@users.noreply.github.com>
2021-06-05 19:19:48 +0430

    Update User Class

--- classes.py
+++ classes.py
@@ -4,7 +4,8 @@ class User :
     def __init__(self, password, name, mail):
         self.password, self.name, self.mail = password, name, mail
         self.wallet = 0
-
+    def setPassword(self, new_password):
+        self.password = new_password
 
 class Seller(User):
   pass
