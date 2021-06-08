139b40f396cc48b2c7cd9b0d98cd84b0ad0a96c5
sina-amareh <79862130+sina-amareh@users.noreply.github.com>
2021-06-08 14:42:29 +0430

    Adding check password function
    to check conditions for a valid password

--- users.py
+++ users.py
@@ -19,4 +19,20 @@ Basic Classes for Shop Objects
 """
 
 class User :
-  pass
+  " CHECK PASSWORD METHOD "
+    def checkPassword(self, password):
+        status = True
+        errors = []
+        if len(password) < 8:
+            status = False
+            errors.append(1)
+        if re.search('[0-9]', password) is None :
+            status = False
+            errors.append(2)
+        if re.search('[A-Z]', password) is None :
+            status = False
+            errors.append(3)
+        if re.search('[a-z]', password) is None :
+            status = False
+            errors.append(4)
+        return {'status': status, 'errors': errors}
