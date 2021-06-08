1276166f5c71fb1cea9b348079129349af7d1670
Mehrun Askarnezhad <78827397+mehrun-askrnzhad@users.noreply.github.com>
2021-06-08 14:40:59 +0430

    Create users.py

---
+++ users.py
@@ -0,0 +1,22 @@
+
+import re
+
+"""
+Basic Classes for Shop Objects 
+
+- User : The Parent Class for Seller and Customer
+- Seller : Seller Users
+- Customer : Customer Users
+- Product : Products
+
+- User :
+-- name     : (str) first_name + last_name
+-- email    : (str) 
+-- birth_year : (int)
+-- password : (str) ( min: 8 char, 1 cap, 1 num  )
+-- wallet   : (int) 
+
+"""
+
+class User :
+  pass
